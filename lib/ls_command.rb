251715d133589ceda3add7eb9facb50603bd7835
taitsulya <iojibka_2000@mail.ru>
2021-08-12 19:21:19 +0300

    Initial commit

---
+++ lib/ls_command.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'colorize'
+require_relative 'ls_command/version'
+
+module LsCommand
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.ls
+    current_dir = Dir.pwd
+    Dir.children(current_dir).map do |entry|
+      if entry[0] == '.'
+        entry.light_black
+      elsif File.directory? File.join(current_dir, entry)
+        entry.blue
+      else
+        entry.white
+      end
+    end
+  end
+end
